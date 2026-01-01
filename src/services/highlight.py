95bd15f9b5dfed9361f62dc2de73841a31f1aed9
nhantran3395 <thanhnhan33.95@gmail.com>
2026-01-01 14:31:40 +0700

    Remove unnecessary logging

--- src/services/highlight.py
+++ src/services/highlight.py
@@ -1,5 +1,4 @@
 from typing import List
-import logging
 
 from ..clients.readwise import ReadwiseClient
 from ..errors import ApiServerError
@@ -7,12 +6,8 @@ from ..models.highlight import Highlight
 
 _readwise_client = ReadwiseClient()
 
-logger = logging.getLogger(__name__)
-
 async def get_highlights() -> List[Highlight]:
     raw_data = await _readwise_client.get_highlights()
-
-    logger.warning(f'get_highlights raw_data: {raw_data}')
 
     if not raw_data or "results" not in raw_data:
         raise ApiServerError("Unexpected response from Readwise API")
